8b3803a42cf84a701274d1645f6987cbaaa6134a
rodrigocamargo <rodrigocamargo854@gmail.com>
2020-09-19 08:14:42 -0300

    Enum

---
+++ AulaEnum_Entites/Enum/OrderStatus.cs
@@ -0,0 +1,10 @@
+namespace AulaEnum_Entites.Enum
+{
+    enum OrderStatus : int
+    {
+        Processing = 0,
+        PendingPayment = 1,
+        Shipped = 2,
+        Delivery = 3
+    };
+}
---
+++ AulaEnum_Entites/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AulaEnum_Entites;
+using AulaEnum_Entites.Enum;
+
+namespace AulaEnum_Entites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Order order = new Order
+           {
+
+               Id = 1080,
+               Moment = DateTime.Now,
+               Status = OrderStatus.PendingPayment 
+
+
+           };
+            Console.WriteLine(order);
+            
+
+        }
+    }
+}
