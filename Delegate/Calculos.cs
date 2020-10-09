e0ad946323e1aa0966e69661751aa80e54837273
rodrigocamargo <rodrigocamargo854@gmail.com>
2020-10-09 07:09:08 -0300

    Delegate

---
+++ Delegate/Calculos.cs
@@ -0,0 +1,16 @@
+namespace Delegate
+{
+    public class Calculos
+    {
+        public int Soma(int n1, int n2){
+            return n1+n2;
+        }
+
+
+        public int Sub(int n1, int n2){
+            return n1-n2;
+        }
+
+        
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+delegate int op(int a, int b);
+
+namespace Delegate
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variavel result recebe o retorno do delgate>>d1
+            var result = 0;
+
+            // cria-se objeto calculos para acesso dos metodos
+            var a = new Calculos();
+            // cria-se objeto do tipo op para acesso do delegate
+            op d1 = new op(a.Soma);
+            op d2 = new op (a.Sub);
+
+            result = d1(5,3);
+            result = d2(7,3);
+
+            //result deve retornar dois valores diferentes
+            // valor 8
+            //valor 4
+
+
+
+            
+        }
+    }
+}
